6f1dd4a7c37b6ab234eecf5cecb5a6837dcbe6f9
MXC <1806148038@qq.com>
2022-08-22 21:18:45 +0800

    first commit

---
+++ src/main/java/com/mxc/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.mxc.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-22 21:15
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
